b936bb4cc87c7bb87ee8c6817ab3f7e8d05c0570
Javier  Matas Pose <86110409+JavierMatasPose@users.noreply.github.com>
2026-01-25 19:01:29 +0100

    Update nvim config

---
+++ nvim.bak/lua/configs/treesitter.lua
@@ -0,0 +1,20 @@
+local options = {
+  ensure_installed = {
+    "c",
+    "python",
+    "markdown",
+    "lua",
+    "vim",
+    "markdown_inline",
+    "vimdoc",
+    "query",
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+}
+
+return options
